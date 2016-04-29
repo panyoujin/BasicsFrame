03dab99e08b446734fae995b2c94fef1756969b0
xuxujiang0 <6895285a>
2016-04-29 15:21:34 +0800

    签入注册接口

--- BF/06 WebAPI/BF.WebAPI/Controllers/MemberController.cs
+++ BF/06 WebAPI/BF.WebAPI/Controllers/MemberController.cs
@@ -43,5 +43,38 @@ namespace BF.WebAPI.Controllers
             apiResult.data = new { Account = user.Account, ImageUrl = user.ImageUrl };
             return JsonHelper.SerializeObjectToWebApi(apiResult);
         }
+
+        [HttpGet]
+        public HttpResponseMessage Register(string account, string passwd)
+        {
+            ApiResult<bool> apiResult = new ApiResult<bool>() { code = ResultCode.CODE_SUCCESS, msg = ResultMsg.CODE_SUCCESS };
+            if (string.IsNullOrWhiteSpace(account) || string.IsNullOrWhiteSpace(passwd))
+            {
+                apiResult.code = ResultCode.CODE_BUSINESS_ERROR;
+                apiResult.msg = "帐号或密码不能为空！";
+                return JsonHelper.SerializeObjectToWebApi(apiResult);
+            }
+            Dictionary<string, object> dic = new Dictionary<string, object>();
+            dic.Add("Account", account);
+            dic.Add("Passwd", passwd);
+            var user = DBBaseFactory.DALBase.QueryForObject<MemberInfo>("FrontApi_GetMemberInfoByAccount", dic);
+            if (user != null)
+            {
+                apiResult.code = ResultCode.CODE_BUSINESS_ERROR;
+                apiResult.msg = "帐号已存在！";
+                return JsonHelper.SerializeObjectToWebApi(apiResult);
+            }
+            int result = DBBaseFactory.DALBase.ExecuteNonQuery("FrontApi_InsertMemberInfo", dic);
+            if (result > 0)
+            {
+                apiResult.data = true;
+            }
+            else
+            {
+                apiResult.code = ResultCode.CODE_BUSINESS_ERROR;
+                apiResult.msg = "保存数据失败！";
+            }
+            return JsonHelper.SerializeObjectToWebApi(apiResult);
+        }
     }
 }
