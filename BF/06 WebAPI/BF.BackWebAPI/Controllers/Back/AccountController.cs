37e291b532374f9b9b60745ebc381d52f9ad89b1
panyoujin <jimmy.pan@hotmail.com>
2016-04-30 23:44:27 +0800

    完成常用模式相关接口开发，把所有post接口修改完毕

--- BF/06 WebAPI/BF.BackWebAPI/Controllers/Back/AccountController.cs
+++ BF/06 WebAPI/BF.BackWebAPI/Controllers/Back/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using BF.BackWebAPI.Models.Back;
+using BF.BackWebAPI.Models.Back.InParam;
 using BF.Common.CommonEntities;
 using BF.Common.DataAccess;
 using BF.Common.Helper;
@@ -11,18 +12,18 @@ namespace BF.BackWebAPI.Controllers.Back
 {
     public class AccountController : BackBaseController
     {
-        [HttpGet]
-        public HttpResponseMessage Login(string account, string password)
+        [HttpPost]
+        public HttpResponseMessage Login([FromBody]Login login)
         {
             ApiResult<object> apiResult = new ApiResult<object>() { code = ResultCode.CODE_SUCCESS, msg = ResultMsg.CODE_SUCCESS };
-            if (string.IsNullOrWhiteSpace(account) || string.IsNullOrWhiteSpace(password))
+            if (string.IsNullOrWhiteSpace(login.account) || string.IsNullOrWhiteSpace(login.password))
             {
                 apiResult.code = ResultCode.CODE_BUSINESS_ERROR;
                 apiResult.msg = "帐号或密码不能为空！";
                 return JsonHelper.SerializeObjectToWebApi(apiResult);
             }
             Dictionary<string, object> dic = new Dictionary<string, object>();
-            dic.Add("UserAccount", account);
+            dic.Add("UserAccount", login.account);
             var user = DBBaseFactory.DALBase.QueryForObject<UserModel>("BackWeb_GetUserByLoginVoucher", dic);
             if (user == null || user.ID <= 0)
             {
@@ -31,7 +32,7 @@ namespace BF.BackWebAPI.Controllers.Back
                 return JsonHelper.SerializeObjectToWebApi(apiResult);
             }
             user.IsAdmin = true;
-            if (user.UserPassword != password)
+            if (user.UserPassword != login.password)
             {
                 apiResult.code = ResultCode.CODE_BUSINESS_ERROR;
                 apiResult.msg = "帐号或密码错误！";
---
+++ BF/06 WebAPI/BF.BackWebAPI/Models/Back/InParam/HealthModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BF.BackWebAPI.Models.Back.InParam
+{
+    public class HealthModel
+    {
+        //string model_Name, string icoUrl = "", string imageUrl = "", string introduce = "", string describe = "", string remarks = "", int sort = 0, bool isBubble = false, int bubble_Time = 0, int bubble_Temperature = 0, int cook_Time = 0, int cook_Temperature = 0, bool is_Heat_Preservation = false, int heat_Preservation_Time = 0, int heat_Preservation_Temperature = 0, int removal_Chlorine_Time = 0, int final_Temperature = 0, bool isFerv = false
+        public string model_Name { get; set; }
+        public string icoUrl { get; set; }
+        public string imageUrl { get; set; }
+        public string introduce { get; set; }
+        public string describe { get; set; }
+        public string remarks { get; set; }
+        public int sort { get; set; }
+        public bool isBubble { get; set; }
+        public int bubble_Time { get; set; }
+        public int bubble_Temperature { get; set; }
+        public int cook_Time { get; set; }
+        public int cook_Temperature { get; set; }
+        public bool is_Heat_Preservation { get; set; }
+        public int heat_Preservation_Time { get; set; }
+        public int heat_Preservation_Temperature { get; set; }
+        public int removal_Chlorine_Time { get; set; }
+        public int final_Temperature { get; set; }
+        public bool isFerv { get; set; }
+    }
+}
