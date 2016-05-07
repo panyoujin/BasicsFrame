0fce028d1efc145abe2f79beb46eaf26498f6be7
xuxujiang0 <6895285a>
2016-05-07 16:57:57 +0800

    提交幻腾只能类

---
+++ BF/02 Commons/BF.Common/Helper/HttpRequestHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BF.Common.Helper
+{
+    public class HttpRequestHelper
+    {
+        public static string Request(string _address, string method = "GET", int timeOut = 5)
+        {
+            string resultJson = string.Empty;
+            if (string.IsNullOrEmpty(_address))
+                return resultJson;
+            Uri address = new Uri(_address);
+
+            // 创建网络请求  
+            HttpWebRequest request = WebRequest.Create(address) as HttpWebRequest;
+
+            // 构建Head
+            request.Method = method;
+            Encoding myEncoding = Encoding.GetEncoding("utf-8");
+            request.Timeout = timeOut * 1000;
+            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+            {
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                string responseStr = reader.ReadToEnd();
+                if (responseStr != null && responseStr.Length > 0)
+                {
+                    resultJson = responseStr;
+                }
+            }
+            return resultJson;
+        }
+    }
+}
---
+++ BF/06 WebAPI/BF.BackWebAPI/Controllers/HTSmartController.cs
@@ -0,0 +1,30 @@
+﻿using BF.Common.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BF.BackWebAPI.Controllers
+{
+    /// <summary>
+    /// 幻腾只能接口
+    /// </summary>
+    public class HTSmartController : BaseController
+    {
+        [HttpGet]
+        public HttpResponseMessage RegisterAppCallBack(string code = "")
+        {
+            if (!string.IsNullOrEmpty(code))
+            {
+                string address = string.Format("https://huantengsmart.com/oauth2/token?client_id={0}&client_secret={1}&redirect_uri={2}&grant_type=authorization_code&code={3}",
+                "6b6b48fcad497527c569fd49b7708294f21609038dd3e9b2f280b42cb7ac4d9b", "729bb36e1e54667cc4d474a57b5801fe10d6980d1b3cd36730d41516cd6ec508", "http%3A%2F%2Fwxtest.bosjk.com%2Fznsh%2FHTSmart%2FRegisterAppCallBack", code);
+
+                string returnStr = HttpRequestHelper.Request(address, "POST", 10);
+            }
+            return null;
+        }
+        //, string error = "", string error_description = "",string access_token="",string token_type="",int expires_in=0,string refresh_token="",string scope="",string created_at=""
+    }
+}
