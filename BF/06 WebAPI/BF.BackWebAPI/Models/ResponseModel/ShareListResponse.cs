025a7de1b7e63144ed3a69d00f274c0631313fb9
panyoujin <jimmy.pan@hotmail.com>
2016-05-30 23:20:33 +0800

    分享接口和分享详情接口添加头像；文章接口更改收藏状态

--- BF/06 WebAPI/BF.BackWebAPI/Models/ResponseModel/ShareListResponse.cs
+++ BF/06 WebAPI/BF.BackWebAPI/Models/ResponseModel/ShareListResponse.cs
@@ -19,6 +19,34 @@ namespace BF.BackWebAPI.Models.ResponseModel
         /// </summary>
         public int User_ID { get; set; }
 
+        private string _user_Image;
+        /// <summary>
+        /// 图片地址
+        /// </summary>
+        public string User_Image
+        {
+            get
+            {
+                if (!string.IsNullOrWhiteSpace(_user_Image))
+                {
+
+                    if (_user_Image.IndexOf("http://") == 0 || _user_Image.IndexOf("https://") == 0)
+                    {
+                        return _user_Image;
+                    }
+                    else
+                    {
+                        return Global.AttmntUrl + _user_Image;
+                    }
+
+                }
+                return _user_Image;
+            }
+            set
+            {
+                _user_Image = value;
+            }
+        }
         /// <summary>
         /// 源ID
         /// </summary>
@@ -64,7 +92,7 @@ namespace BF.BackWebAPI.Models.ResponseModel
         public int MyPraiseCount { get; set; }
 
         public string AttachUrls { get; set; }
-        
+
         private List<string> _imgUrlList;
         /// <summary>
         /// 图片地址
@@ -79,7 +107,7 @@ namespace BF.BackWebAPI.Models.ResponseModel
                     var urls = AttachUrls.Split(',');
                     foreach (var item in urls)
                     {
-                        if(item.IndexOf("http://")==0|| item.IndexOf("https://") == 0)
+                        if (item.IndexOf("http://") == 0 || item.IndexOf("https://") == 0)
                         {
                             _imgUrlList.Add(item);
                         }
