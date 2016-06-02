b533ddc80e0aff6406e59c7f9960356cf5f0c6b5
panyoujin <jimmy.pan@hotmail.com>
2016-06-02 10:15:28 +0800

    修改点击样式

--- BF/06 WebAPI/BF.BackWebAPI/WebPage/js/pagination.js
+++ BF/06 WebAPI/BF.BackWebAPI/WebPage/js/pagination.js
@@ -17,80 +17,6 @@
         //ptotal = Math.ceil(total / psize);
         callback(pindex, psize);
         return;
-        //ptotal = Math.ceil(total / psize);
-        //var page_html = "<ul><li data='Prev'><a href='#'>Prev</a></li>";
-        //if (ptotal > 10) {
-        //    if (pindex <= 6) {
-        //        for (var i = 0; i < ptotal; i++) {
-        //            if ((i < 6 && i != (pindex - 1)) || i >= ptotal - 2) {
-        //                page_html += "<li data='" + (i + 1) + "'><a href='#'>" + (i + 1) + "</a></li>";
-        //            } else if (i == (pindex - 1)) {
-        //                page_html += "<li data='" + (i + 1) + "'><a href='#'>" + (i + 1) + "</a></li>";
-        //            } else {
-        //                i = ptotal - 3;
-        //                page_html += "<li data=''><a href='#'>...</a></li>";
-        //            }
-        //        }
-        //    } else if (pindex > 6 && pindex <= 8) {
-        //        for (var i = 0; i < ptotal; i++) {
-        //            if ((i < 8 && i != (pindex - 1)) || i >= ptotal - 2) {
-        //                page_html += "<li data='" + (i + 1) + "'><a href='#'>" + (i + 1) + "</a></li>";
-        //            } else if (i == (pindex - 1)) {
-        //                page_html += "<li data='" + (i + 1) + "'><a href='#'>" + (i + 1) + "</a></li>";
-        //            } else {
-        //                i = ptotal - 3;
-        //                page_html += "<li data=''><a href='#'>...</a></li>";
-        //            }
-        //        }
-        //    } else if (pindex >= ptotal - 5) {
-        //        for (var i = 0; i < ptotal; i++) {
-        //            if (i < 2 || (i > (pindex - 3) && i != (pindex - 1))) {
-        //                page_html += "<li data='" + (i + 1) + "'><a href='#'>" + (i + 1) + "</a></li>";
-        //            } else if (i == (pindex - 1)) {
-        //                page_html += "<li data='" + (i + 1) + "'><a href='#'>" + (i + 1) + "</a></li>";
-        //            } else {
-        //                i = pindex - 4;
-        //                page_html += "<li data=''><a href='#'>...</a></li>";
-        //            }
-        //        }
-        //    } else {
-        //        for (var i = 0; i < ptotal; i++) {
-        //            if (i < 2 || i > ptotal - 2) {
-        //                page_html += "<li data='" + (i + 1) + "'><a href='#'>" + (i + 1) + "</a></li>";
-        //            } else if (i > pindex - 3 && i < pindex + 2 && i != (pindex - 1)) {
-        //                page_html += "<li data='" + (i + 1) + "'><a href='#'>" + (i + 1) + "</a></li>";
-        //            } else if (i == (pindex - 1)) {
-        //                page_html += "<li data='" + (i + 1) + "'><a href='#'>" + (i + 1) + "</a></li>";
-        //            } else if (i < pindex) {
-        //                i = pindex - 4;
-        //                page_html += "<li data=''><a href='#'>...</a></li>";
-        //            } else {
-        //                i = ptotal - 3;
-        //                page_html += "<li data=''><a href='#'>...</a></li>";
-        //            }
-        //        }
-        //    }
-        //} else {
-        //    for (var i = 0; i < ptotal; i++) {
-        //        page_html += "<li data='" + (i + 1) + "'><a href='#'>" + (i + 1) + "</a></li>";
-        //    }
-        //}
-        //page_html += " <li data='Next'><a href='#'>Next</a></li></ul>";
-        //$("#pagination").html(page_html);
-        //$("#pagination li").off('click');
-        //$("#pagination li").on('click', function () {
-        //    var c_value = $(this).attr("data");
-        //    if (c_value == "Prev") {
-        //        pindex = (pindex - 1);
-        //    } else if (c_value == "Next") {
-        //        pindex = (pindex + 1);
-        //    } else if (c_value == null || c_value.length <= 0) {
-        //        return;
-        //    } else {
-        //        pindex = c_value;
-        //    }
-        //    callback(pindex, psize);
-        //});
     }
 
 
@@ -101,14 +27,12 @@
         if (size > 0) {
             psize = size;
         }
-        //if (total) {
-        //    pmaxindex = Math.ceil(total / psize);
-        //    ptotal = total;
-        //}
         ptotal = Math.ceil(total / psize);
         var page_html = "<ul>";
         if (pindex > 1) {
             page_html += "<li data='Prev'><a href='#'>Prev</a></li>";
+        } else {
+            page_html += "<li data='Prev' class='disabled'><a href='#'>Prev</a></li>";
         }
         if (ptotal > 10) {
             if (pindex < 5) {
@@ -116,7 +40,7 @@
                     if ((i < 6 && i != ((parseInt(pindex) - 1))) || i >= ptotal - 2) {
                         page_html += "<li data='" + (i + 1) + "'><a href='#'>" + (i + 1) + "</a></li>";
                     } else if (i == (pindex - 1)) {
-                        page_html += "<li data='" + (i + 1) + "'><a href='#'>" + (i + 1) + "</a></li>";
+                        page_html += "<li data='" + (i + 1) + "' class='active'><a href='#'>" + (i + 1) + "</a></li>";
                     } else {
                         i = ptotal - 3;
                         page_html += "<li data=''><a href='#'>...</a></li>";
@@ -127,7 +51,7 @@
                     if ((i < (parseInt(pindex) + 2) && i != (parseInt(pindex) - 1)) || i >= ptotal - 2) {
                         page_html += "<li data='" + (i + 1) + "'><a href='#'>" + (i + 1) + "</a></li>";
                     } else if (i == (pindex - 1)) {
-                        page_html += "<li data='" + (i + 1) + "'><a href='#'>" + (i + 1) + "</a></li>";
+                        page_html += "<li data='" + (i + 1) + "' class='active'><a href='#'>" + (i + 1) + "</a></li>";
                     } else {
                         i = ptotal - 3;
                         page_html += "<li data=''><a href='#'>...</a></li>";
@@ -138,7 +62,7 @@
                     if (i < 2 || (i > (pindex - 3) && i != (pindex - 1))) {
                         page_html += "<li data='" + (i + 1) + "'><a href='#'>" + (i + 1) + "</a></li>";
                     } else if (i == (pindex - 1)) {
-                        page_html += "<li data='" + (i + 1) + "'><a href='#'>" + (i + 1) + "</a></li>";
+                        page_html += "<li data='" + (i + 1) + "' class='active'><a href='#'>" + (i + 1) + "</a></li>";
                     } else {
                         i = pindex - 4;
                         page_html += "<li data=''><a href='#'>...</a></li>";
@@ -151,7 +75,7 @@
                     } else if (i > pindex - 3 && i < (parseInt(pindex) + 2) && i != (pindex - 1)) {
                         page_html += "<li data='" + (i + 1) + "'><a href='#'>" + (i + 1) + "</a></li>";
                     } else if (i == (pindex - 1)) {
-                        page_html += "<li data='" + (i + 1) + "'><a href='#'>" + (i + 1) + "</a></li>";
+                        page_html += "<li data='" + (i + 1) + "' class='active'><a href='#'>" + (i + 1) + "</a></li>";
                     } else if (i < pindex) {
                         i = pindex - 4;
                         page_html += "<li data=''><a href='#'>...</a></li>";
@@ -163,10 +87,17 @@
             }
         } else {
             for (var i = 0; i < ptotal; i++) {
-                page_html += "<li data='" + (i + 1) + "'><a href='#'>" + (i + 1) + "</a></li>";
+                if (i == (pindex - 1)) {
+                    page_html += "<li data='" + (i + 1) + "' class='active'><a href='#'>" + (i + 1) + "</a></li>";
+                } else {
+                    page_html += "<li data='" + (i + 1) + "'><a href='#'>" + (i + 1) + "</a></li>";
+                }
             }
-        } if (pindex < ptotal) {
+        }
+        if (pindex < ptotal) {
             page_html += "<li data='Next'><a href='#'>Next</a></li>";
+        } else {
+            page_html += "<li data='Next' class='disabled'><a href='#'>Next</a></li>";
         }
         page_html += " </ul>";
         //alert(page_html);
@@ -175,17 +106,10 @@
         $("#pagination li").on('click', function () {
             var c_value = $(this).attr("data");
             if (c_value == "Prev") {
-                if (pindex > 1) {
                     pindex = (pindex - 1);
-                } else {
-                    return;
-                }
             } else if (c_value == "Next") {
-                if (pindex < ptotal - 1) {
                     pindex = (pindex + 1);
-                } else {
-                    return;
-                }
+                
             } else if (c_value == null || c_value.length <= 0) {
                 return;
             } else {
