6bd2d9083e5479a811e4fbd882bc86b222839cbd
xuxujiang0 <6895285a>
2016-06-12 20:34:18 +0800

    添加用户controller

---
+++ BF/06 WebAPI/BF.BackWebAPI/Controllers/Back/Back_MemberController.cs
@@ -0,0 +1,55 @@
+﻿using BF.Common.CommonEntities;
+using BF.Common.DataAccess;
+using BF.Common.Helper;
+using BF.Common.StaticConstant;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BF.BackWebAPI.Controllers.Back
+{
+    public class Back_MemberController : ApiController
+    {
+        public HttpResponseMessage GetMemberInfos(string search = "", int page = CommonConstant.PAGE, int pageSize = CommonConstant.PAGE_SIZE)
+        {
+            ApiResult<object> apiResult = new ApiResult<object>() { code = ResultCode.CODE_SUCCESS, msg = ResultMsg.CODE_SUCCESS };
+            if (page <= 0)
+            {
+                page = 1;
+            }
+            if (pageSize <= 0)
+            {
+                pageSize = 10;
+            }
+            var startSize = (page > 1 ? (page - 1) * pageSize : 0);
+            Dictionary<string, object> dic = new Dictionary<string, object>();
+            dic.Add("StartSize", startSize);
+            dic.Add("PageSize", pageSize);
+            if (!string.IsNullOrWhiteSpace(search))
+            {
+                dic.Add("Search", "%" + search + "%");
+            }
+
+            DataSet ds = DBBaseFactory.DALBase.QueryForDataSet("Back_GetShopsList", dic);
+
+            if (ds != null && ds.Tables.Count >= 2)
+            {
+                foreach (DataRow item in ds.Tables[0].Rows)
+                {
+                    if (item["ImageUrl"] != null && !string.IsNullOrWhiteSpace(item["ImageUrl"].ToString()))
+                    {
+                        item["ImageUrl"] = Global.AttmntUrl + item["ImageUrl"].ToString();
+                    }
+                }
+                var result = new { table = ds.Tables[0], total = ds.Tables[1].Rows[0][0] };
+                apiResult.data = result;
+            }
+
+            return JsonHelper.SerializeObjectToWebApi(apiResult);
+        }
+    }
+}
