9f5b0e59902b63682c7254ba7109df2da4399a90
xuxujiang0 <6895285a>
2016-10-10 22:58:10 +0800

    后台新增社区分享

---
+++ BF/06 WebAPI/BF.BackWebAPI/Controllers/Back/Back_ShareController.cs
@@ -0,0 +1,64 @@
+﻿using BF.BackWebAPI.Models.Back.InParam;
+using BF.BackWebAPI.Models.Back.OutParam;
+using BF.Common.CommonEntities;
+using BF.Common.DataAccess;
+using BF.Common.FileProcess;
+using BF.Common.Helper;
+using BF.Common.StaticConstant;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Http;
+
+namespace BF.BackWebAPI.Controllers.Back
+{
+    public class Back_ShareController : ApiController
+    {
+     
+        public HttpResponseMessage GetUserShares(string search = "", int page = CommonConstant.PAGE, int pageSize = CommonConstant.PAGE_SIZE)
+        {
+            ApiResult<object> apiResult = new ApiResult<object>() { code = ResultCode.CODE_SUCCESS, msg = ResultMsg.CODE_SUCCESS };
+            if (page <= 0)
+            {
+                page = 1;
+            }
+            if (pageSize <= 0)
+            {
+                pageSize = 10;
+            }
+            var startSize = (page > 1 ? (page - 1) * pageSize : 0);
+            Dictionary<string, object> dic = new Dictionary<string, object>();
+            dic.Add("StartSize", startSize);
+            dic.Add("PageSize", pageSize);
+            if (!string.IsNullOrWhiteSpace(search))
+            {
+                dic.Add("Search", "%" + search + "%");
+            }
+
+            DataSet ds = DBBaseFactory.DALBase.QueryForDataSet("BackWeb_GetAllUserShare", dic);
+
+            if (ds != null && ds.Tables.Count >= 2)
+            {
+                //foreach (DataRow item in ds.Tables[0].Rows)
+                //{
+                //    if (item["ImageUrl"] != null && !string.IsNullOrWhiteSpace(item["ImageUrl"].ToString()))
+                //    {
+                //        item["FullUrl"] = Global.AttmntUrl + item["ImageUrl"].ToString();
+                //    }
+                //}
+                var result = new { table = ds.Tables[0], total = ds.Tables[1].Rows[0][0] };
+                apiResult.data = result;
+            }
+
+            return JsonHelper.SerializeObjectToWebApi(apiResult);
+        }
+
+
+    }
+}
---
+++ BF/06 WebAPI/BF.BackWebAPI/modules/UserShares.js
@@ -0,0 +1,62 @@
+﻿define(['domready!', 'zepto', 'common', 'angular', 'server_data/models_data', 'pagination'], function (doc, $, c, angular, m, p) {
+    $(function () {
+        loadData(1, 10);
+        $("#btn_Search").click(function () { loadData(); });
+        $("#new_Advertise").click(function () { window.location.href = "/Advertise_Detail.html"; });
+    })
+
+
+
+    function loadData(page, size) {
+
+        var param = {
+            search: $("#text_search").val(), page: page, pageSize: size
+        };
+
+        var url = window.apibase + "/Back_Share/GetUserShares";
+        c.get(param, url, function (data) {
+            init_html(data);
+            p.setindex(page, size, data.data.total, function (pindex, psize) {
+                loadData(pindex, psize);
+            });
+        })
+    }
+
+
+
+    function init_html(data) {
+        if (data != null && data.code == "200" && data.data != null) {
+            $("#tab").html("");
+            console.debug(data);
+            for (var i = 0; i < data.data.table.length; i++) {
+                var str = "<tr><td>" + (i + 1) + "</td>";
+                str += "<td>" + data.data.table[i].ShareTitle + "</td>";
+                str += "<td>" + data.data.table[i].ShareContent + "</td>";
+                str += "<td>" + data.data.table[i].Source_TypeStr + "</td>";
+                str += "<td>" + data.data.table[i].Account + "</td>";
+                str += "<td>" + data.data.table[i].Name + "</td>";
+                str += "<td>" + data.data.table[i].CreationDate + "</td>";
+                str += "<td>" + data.data.table[i].IsHotStr + "</td>";
+                str += "<td>" + data.data.table[i].HotSort + "</td>";
+
+                str += '<td><a href="Advertise_Detail.html?ID=' + data.data.table[i].ID + '" ><i class="icon-pencil">编辑</i></a>  <a href="#" role="button"  data-toggle="modal" id="btnDelete" data-id="' + data.data.table[i].ID + '" data-name="' + data.data.table[i].Name + '"><i class="icon-remove">删除</i></a> </td></tr>';
+                $("#tab").append(str);
+            }
+            $("td #btnDelete").off("click");
+            $('td #btnDelete').on('click', function () {
+
+                if (confirm('确定要删除_' + $(this).attr("data-name") + '_吗?')) {
+                    var url = window.apibase + "/Back_Advertise/DeleteAdvertiseByID";
+                    c.get({ ID: $(this).attr("data-id") }, url, function (data) {
+                        if (data != null && data.code == "200") {
+                            loadData(1, 10);
+                        }
+                    })
+                } else {
+                    return false;
+                }
+            });
+        }
+    }
+
+})
