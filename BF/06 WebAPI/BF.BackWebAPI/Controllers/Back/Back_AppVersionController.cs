df971c625b499eeaebb005c0f65e95f8a3914675
xuxujiang0 <6895285a>
2016-10-18 11:54:22 +0800

    新增版本管理

---
+++ BF/06 WebAPI/BF.BackWebAPI/Controllers/Back/Back_AppVersionController.cs
@@ -0,0 +1,84 @@
+﻿using BF.BackWebAPI.Models.Back.InParam;
+using BF.BackWebAPI.Models.Back.OutParam;
+using BF.Common.CommonEntities;
+using BF.Common.DataAccess;
+using BF.Common.FileProcess;
+using BF.Common.Helper;
+using BF.Common.StaticConstant;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Http;
+
+namespace BF.BackWebAPI.Controllers.Back
+{
+    public class Back_AppVersionController : BaseController
+    {
+
+        public HttpResponseMessage GetAppVersions(int page = CommonConstant.PAGE, int pageSize = CommonConstant.PAGE_SIZE)
+        {
+            ApiResult<object> apiResult = new ApiResult<object>() { code = ResultCode.CODE_SUCCESS, msg = ResultMsg.CODE_SUCCESS };
+            if (page <= 0)
+            {
+                page = 1;
+            }
+            if (pageSize <= 0)
+            {
+                pageSize = 10;
+            }
+            var startSize = (page > 1 ? (page - 1) * pageSize : 0);
+            Dictionary<string, object> dic = new Dictionary<string, object>();
+            dic.Add("StartSize", startSize);
+            dic.Add("PageSize", pageSize);
+
+
+            DataSet ds = DBBaseFactory.DALBase.QueryForDataSet("BackWeb_GetAppVersion", dic);
+
+            if (ds != null && ds.Tables.Count >= 2)
+            {
+                //foreach (DataRow item in ds.Tables[0].Rows)
+                //{
+                //    if (item["ImageUrl"] != null && !string.IsNullOrWhiteSpace(item["ImageUrl"].ToString()))
+                //    {
+                //        item["FullUrl"] = Global.AttmntUrl + item["ImageUrl"].ToString();
+                //    }
+                //}
+                var result = new { table = ds.Tables[0], total = ds.Tables[1].Rows[0][0] };
+                apiResult.data = result;
+            }
+
+            return JsonHelper.SerializeObjectToWebApi(apiResult);
+        }
+
+        [HttpGet]
+        public HttpResponseMessage InsertAppVersion(string text_Version, int text_AppType, string text_Target_Url, int text_Update_Status, string text_VersionDate, int text_FileSize, string text_UpdateContent)
+        {
+            ApiResult<object> apiResult = new ApiResult<object>() { code = ResultCode.CODE_SUCCESS, msg = ResultMsg.CODE_SUCCESS };
+
+            Dictionary<string, object> dic = new Dictionary<string, object>();
+            dic.Add("Version", text_Version);
+            dic.Add("Update_Status", text_Update_Status);
+            dic.Add("Target_Url", text_Target_Url);
+            dic.Add("AppType", text_AppType);
+            dic.Add("VersionDate", text_VersionDate);
+            dic.Add("FileSize", text_FileSize);
+            dic.Add("CreationUser", MemberInfo.Account);
+            dic.Add("UpdateContent", text_UpdateContent);
+            int result = DBBaseFactory.DALBase.ExecuteNonQuery("BackWeb_InsertAppVersion", dic);
+            if (result <= 0)
+            {
+                apiResult.code = ResultCode.CODE_UPDATE_FAIL;
+                apiResult.msg = ResultMsg.CODE_EXCEPTION;
+            }
+
+
+            return JsonHelper.SerializeObjectToWebApi(apiResult);
+        }
+    }
+}
---
+++ BF/06 WebAPI/BF.BackWebAPI/modules/AppVersion.js
@@ -0,0 +1,93 @@
+﻿define(['domready!', 'zepto', 'common', 'angular', 'server_data/models_data', 'pagination'], function (doc, $, c, angular, m, p) {
+    $(function () {
+        loadData(1, 10);
+
+        $("#btn_save").click(function () {
+
+            var param = {
+                text_Version: $("#text_Version").val(), text_AppType: $("#text_AppType").val(), text_Target_Url: $("#text_Target_Url").val(), text_Update_Status: $("#text_Update_Status").val(),
+                text_VersionDate: $("#text_VersionDate").val(), text_FileSize: $("#text_FileSize").val(), text_UpdateContent: $("#text_UpdateContent").val()
+            };
+            console.debug(param);
+            var url = window.apibase + "/Back_AppVersion/InsertAppVersion";
+
+            c.get(param, url, function (data) {
+                if (data != null && data.code == "200") {
+
+                    $('#myModal').modal('hide')
+
+                    loadData(1, 10);
+                }
+
+            })
+            loadData();
+        });
+    })
+
+
+    function loadData(page, size) {
+
+        var param = {
+            page: page, pageSize: size
+        };
+
+        var url = window.apibase + "/Back_AppVersion/GetAppVersions";
+        c.get(param, url, function (data) {
+            init_html(data);
+            p.setindex(page, size, data.data.total, function (pindex, psize) {
+                loadData(pindex, psize);
+            });
+        })
+    }
+
+    function init_html(data) {
+        if (data != null && data.code == "200" && data.data != null) {
+            $("#tab").html("");
+
+            for (var i = 0; i < data.data.table.length; i++) {
+                var str = "<tr><td>" + (i + 1) + "</td>";
+                str += "<td>" + data.data.table[i].Version + "</td>";
+                str += "<td>" + data.data.table[i].Target_Url + "</td>";
+                if (data.data.table[i].AppType == 0) {
+                    str += "<td>Android</td>";
+                } else {
+                    str += "<td>IOS</td>";
+                }
+                if (data.data.table[i].Update_Status == 0) {
+                    str += "<td>不需要更新</td>";
+                } else if (data.data.table[i].Update_Status == 1) {
+                    str += "<td>有更新但非必要</td>";
+                } else {
+                    str += "<td>强制更新</td>";
+                }
+                    
+                str += "<td>" + data.data.table[i].VersionDate + "</td>";
+                str += "<td>" + data.data.table[i].CreationDate + "</td>";
+                str += "<td>" + data.data.table[i].CreationUser + "</td>";
+                str += "<td>" + data.data.table[i].FileSize + "</td>";
+                str += "<td>" + data.data.table[i].UpdateContent + "</td>";
+                str += '<td><a href="#" role="button" id="btnDelete" data-id="' + data.data.table[i].ID + '" >删除</a> ';
+                str += '</td></tr>';
+                $("#tab").append(str);
+            }
+            $("td #btnDelete").off("click");
+            $('td #btnDelete').on('click', function () {
+                alert($(this).attr("data-id"));
+                //var param = { id: $(this).attr("data-id"), hot: $(this).attr("data-hot") };
+
+                //var url = window.apibase + "/Back_Share/SetHot";
+
+                //c.get(param, url, function (data) {
+                //    if (data != null && data.code == "200") {
+                //        loadData(1, 10);
+                //    }
+
+                //})
+            });
+
+   
+
+        }
+    }
+
+})
